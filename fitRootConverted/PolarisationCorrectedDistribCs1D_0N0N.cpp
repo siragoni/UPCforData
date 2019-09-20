c74bd22c650613a969c9d1c32065050fe8d0f1e4
siragoni <simone.ragoni@cern.ch>
2019-09-20 12:54:03 +0100

    Polarisation for 0N0N: framework completed (only bash script remains).

---
+++ fitRootConverted/PolarisationCorrectedDistribCs1D_0N0N.cpp
@@ -0,0 +1,132 @@
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TLeaf.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TLatex.h"
+using namespace std;
+#include <math.h>
+#include <vector>
+
+#include "TDatime.h"
+
+
+#include "TH2.h"
+
+//_____________________________________________________________________________
+/* - Fit function for the ZNC plots.
+ * -
+ */
+void PolarisationCorrectedDistribCs1D_0N0N(){
+
+  TFile* fileList = new TFile("MCtrainResults/2019-09-17/kCohJpsiToMu/AnalysisResults.root");
+  TDirectory* dir = fileList->GetDirectory("MyTask");
+  TList* listings;
+  dir->GetObject("MyOutputContainer", listings);
+  /* - We now do the same as before to ascertain if the TList was there and
+   * - to try to retrieve the plots. Result:
+   *   listings->ls()
+   *     OBJ: TList	  MyOutputContainer	          Doubly linked list          : 0
+   *     OBJ: TH1F	  fNumberMuonsH	              fNumberMuonsH               : 0 at: 0x5a145f0
+   *     OBJ: TH1F	  fCounterH	                  fCounterH                   : 0 at: 0x5a3b570
+   *     OBJ: TH1F	  fEtaMuonH	                  fEtaMuonH                   : 0 at: 0x5a3ba80
+   *     OBJ: TH1F	  fRAbsMuonH	                fRAbsMuonH                  : 0 at: 0x5a3c0c0
+   *     OBJ: TH1F	  fInvariantMassDistributionH	fInvariantMassDistributionH : 0 at: 0x5a3c720
+   */
+  TH1F* fReconCosThetaH = (TH1F*)listings->FindObject("fCosThetaCsFrameTwentyfiveBinsH");
+  TH1F* fGenerCosThetaH = (TH1F*)listings->FindObject("fMCCosThetaCsFrameTwentyfiveBinsH");
+  fReconCosThetaH->Sumw2();
+  fGenerCosThetaH->Sumw2();
+  TH1F* ReconTheta = (TH1F*) fReconCosThetaH->Clone("ReconTheta");
+
+  TH1F* fReconPhiH = (TH1F*)listings->FindObject("fPhiCsFrameTwentyfiveBinsH");
+  TH1F* fGenerPhiH = (TH1F*)listings->FindObject("fMCPhiCsFrameTwentyfiveBinsH");
+  fReconPhiH->Sumw2();
+  fGenerPhiH->Sumw2();
+
+  TH1F* fReconTildePhiH = (TH1F*)listings->FindObject("fTildePhiCsFrameTwentyfiveBinsH");
+  TH1F* fGenerTildePhiH = (TH1F*)listings->FindObject("fMCTildePhiCsFrameTwentyfiveBinsH");
+  fReconTildePhiH->Sumw2();
+  fGenerTildePhiH->Sumw2();
+
+  // TFile* fileDataRawCosTheta = new TFile("pngResults/CosThetaCsFrame.root");
+  // TFile* fileDataRawPhi      = new TFile("pngResults/PhiCsFrame.root");
+  // TFile* fileDataRawTildePhi = new TFile("pngResults/TildePhiCsFrame.root");
+  TDatime d;
+  TFile* fileDataRawCosTheta = new TFile( Form("pngResults/%d-%2.2d-%2.2d/CosThetaCS_0N0N/CosThetaCSFrame_0N0N.root", d.GetYear(), d.GetMonth(), d.GetDay() ) );
+  TFile* fileDataRawPhi      = new TFile( Form("pngResults/%d-%2.2d-%2.2d/PhiCS_0N0N/PhiCsFrame_0N0N.root",           d.GetYear(), d.GetMonth(), d.GetDay() ) );
+  TFile* fileDataRawTildePhi = new TFile( Form("pngResults/%d-%2.2d-%2.2d/TildePhiCS_0N0N/TildePhiCsFrame_0N0N.root", d.GetYear(), d.GetMonth(), d.GetDay() ) );
+
+  TH1F* CosThetaAfterSignalExtractionErrorsRawH = (TH1F*)fileDataRawCosTheta->Get("CosThetaAfterSignalExtractionErrorsH");
+  CosThetaAfterSignalExtractionErrorsRawH->Sumw2();
+  TH1F* RawCosThetaH = (TH1F*) CosThetaAfterSignalExtractionErrorsRawH->Clone("RawCosThetaH");
+
+  TH1F* PhiAfterSignalExtractionErrorsRawH = (TH1F*)fileDataRawPhi->Get("PhiAfterSignalExtractionErrorsH");
+  PhiAfterSignalExtractionErrorsRawH->Sumw2();
+  TH1F* RawPhiH = (TH1F*) PhiAfterSignalExtractionErrorsRawH->Clone("RawPhiH");
+
+  TH1F* TildePhiAfterSignalExtractionErrorsRawH = (TH1F*)fileDataRawTildePhi->Get("TildePhiAfterSignalExtractionErrorsH");
+  TildePhiAfterSignalExtractionErrorsRawH->Sumw2();
+  TH1F* RawTildePhiH = (TH1F*) TildePhiAfterSignalExtractionErrorsRawH->Clone("RawTildePhiH");
+
+
+  TCanvas* AcceptanceCanvasCosTheta = new TCanvas("AcceptanceCanvasCosTheta","AcceptanceCanvasCosTheta",900,800);
+  TH1F* acceptanceCosTheta = (TH1F*) fReconCosThetaH->Clone("acceptanceCosTheta");
+  acceptanceCosTheta->Divide(fGenerCosThetaH);
+  acceptanceCosTheta->Draw("ep");
+
+  TCanvas* CorrCanvasCosTheta = new TCanvas("CorrCanvasCosTheta","CorrCanvasCosTheta",900,800);
+  RawCosThetaH->Divide(acceptanceCosTheta);
+  RawCosThetaH->Draw("ep");
+
+  TCanvas* AcceptanceCanvasPhi = new TCanvas("AcceptanceCanvasPhi","AcceptanceCanvasPhi",900,800);
+  TH1F* acceptancePhi = (TH1F*) fReconPhiH->Clone("acceptancePhi");
+  acceptancePhi->Divide(fGenerPhiH);
+  acceptancePhi->Draw("ep");
+
+  TCanvas* CorrCanvasPhi = new TCanvas("CorrCanvasPhi","CorrCanvasPhi",900,800);
+  RawPhiH->Divide(acceptancePhi);
+  RawPhiH->Draw("ep");
+
+  TCanvas* AcceptanceCanvasTildePhi = new TCanvas("AcceptanceCanvasTildePhi","AcceptanceCanvasTildePhi",900,800);
+  TH1F* acceptanceTildePhi = (TH1F*) fReconTildePhiH->Clone("acceptanceTildePhi");
+  acceptanceTildePhi->Divide(fGenerTildePhiH);
+  acceptanceTildePhi->Draw("ep");
+
+  TCanvas* CorrCanvasTildePhi = new TCanvas("CorrCanvasTildePhi","CorrCanvasTildePhi",900,800);
+  RawTildePhiH->Divide(acceptanceTildePhi);
+  RawTildePhiH->Draw("ep");
+
+
+  TH1F* CorrCosThetaH = (TH1F*) RawCosThetaH->Clone("CorrCosThetaH");
+  TH1F* CorrPhiH      = (TH1F*) RawPhiH     ->Clone("CorrPhiH");
+  TH1F* CorrTildePhiH = (TH1F*) RawTildePhiH->Clone("CorrTildePhiH");
+
+  // TH1F* AccErrors = new TH1F("AccErrors", "AccErrors", 40, -1, 1);
+  // TH1F* EntErrors = new TH1F("EntErrors", "EntErrors", 40, -1, 1);
+  // for( Int_t iLoop = 1; iLoop <= 40; iLoop++ ){
+  //   Double_t AcceptanceEntries = ReconTheta        ->GetBinContent(iLoop);
+  //   Double_t AcceptanceValue   = acceptanceCosTheta->GetBinContent(iLoop);
+  //   if( AcceptanceEntries >= 1  ){
+  //     AccErrors->SetBinContent( iLoop, AcceptanceValue/(TMath::Sqrt(AcceptanceEntries)) );
+  //     EntErrors->SetBinContent( iLoop, 1/(TMath::Sqrt(AcceptanceEntries)) );
+  //   }
+  // }
+
+
+  TFile f("pngResults/PolarisationCorrectedCs1D_0N0N.root", "recreate");
+  acceptanceCosTheta->Write();
+  CorrCosThetaH     ->Write();
+  acceptancePhi     ->Write();
+  CorrPhiH          ->Write();
+  acceptanceTildePhi->Write();
+  CorrTildePhiH     ->Write();
+  // AccErrors         ->Write();
+  // EntErrors         ->Write();
+  // ReconTheta        ->Write();
+  f.Close();
+}
---
+++ fitRootConverted/PolarisationCorrectedDistribHe2D_0N0N.cpp
@@ -0,0 +1,78 @@
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TLeaf.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TLatex.h"
+using namespace std;
+#include <math.h>
+#include <vector>
+
+
+#include "TDatime.h"
+
+#include "TH2.h"
+
+//_____________________________________________________________________________
+/* - Fit function for the ZNC plots.
+ * -
+ */
+void PolarisationCorrectedDistribHe2D_0N0N(){
+
+  TFile* fileList = new TFile("MCtrainResults/2019-09-17/kCohJpsiToMu/AnalysisResults.root");
+  TDirectory* dir = fileList->GetDirectory("MyTask");
+  TList* listings;
+  dir->GetObject("MyOutputContainer", listings);
+  /* - We now do the same as before to ascertain if the TList was there and
+   * - to try to retrieve the plots. Result:
+   *   listings->ls()
+   *     OBJ: TList	  MyOutputContainer	          Doubly linked list          : 0
+   *     OBJ: TH1F	  fNumberMuonsH	              fNumberMuonsH               : 0 at: 0x5a145f0
+   *     OBJ: TH1F	  fCounterH	                  fCounterH                   : 0 at: 0x5a3b570
+   *     OBJ: TH1F	  fEtaMuonH	                  fEtaMuonH                   : 0 at: 0x5a3ba80
+   *     OBJ: TH1F	  fRAbsMuonH	                fRAbsMuonH                  : 0 at: 0x5a3c0c0
+   *     OBJ: TH1F	  fInvariantMassDistributionH	fInvariantMassDistributionH : 0 at: 0x5a3c720
+   */
+  TH2F* fReconH = (TH2F*)listings->FindObject("fCosThetaAndPhiHelicityFrameMyBinningH");
+  TH2F* fGenerH = (TH2F*)listings->FindObject("fMCCosThetaAndPhiHelicityFrameMyBinningH");
+  fReconH->Sumw2();
+  fGenerH->Sumw2();
+
+  TDatime d;
+  TFile* fileDataRaw = new TFile(Form("pngResults/%d-%2.2d-%2.2d/2DHE_0N0N/Polarisation2DHE_0N0N.root", d.GetYear(), d.GetMonth(), d.GetDay() ) );
+  // TFile* fileDataRaw = new TFile("pngResults/2019-09-18/2DHE/Polarisation2DHE.root");
+  TH2F* helicity2DafterSignalExtractionErrors = (TH2F*)fileDataRaw->Get("helicity2DafterSignalExtractionErrors");
+  helicity2DafterSignalExtractionErrors->Sumw2();
+  TH2F* RawH = (TH2F*) helicity2DafterSignalExtractionErrors->Clone("RawH");
+
+
+  TCanvas* AcceptanceCanvas = new TCanvas("AcceptanceCanvas","AcceptanceCanvas",900,800);
+  TH2F* acceptance = (TH2F*) fReconH->Clone("acceptance");
+  acceptance->Divide(fGenerH);
+  acceptance->Draw("ep");
+
+  TCanvas* CorrCanvas = new TCanvas("CorrCanvas","CorrCanvas",900,800);
+  RawH->Divide(acceptance);
+  RawH->Draw("colZ");
+  TCanvas* CorrCanvas2 = new TCanvas("CorrCanvas2","CorrCanvas2",900,800);
+  RawH->Draw("ep");
+
+  // TH1F* AccErrors = new TH1F("AccErrors", "AccErrors", 40, -1, 1);
+  // for( Int_t iLoop = 1; iLoop <= 40; iLoop++ ){
+  //   Double_t AcceptanceEntries = fReconH   ->GetBinContent(iLoop);
+  //   Double_t AcceptanceValue   = acceptance->GetBinContent(iLoop);
+  //   if( AcceptanceEntries >= 1  ){
+  //     AccErrors->SetBinContent( iLoop, AcceptanceValue/TMath::Sqrt(AcceptanceEntries) );
+  //   }
+  // }
+
+  TFile f("pngResults/PolarisationCorrectedHe2D_0N0N.root", "recreate");
+  acceptance->Write();
+  RawH      ->Write();
+  // AccErrors ->Write();
+  f.Close();
+}
